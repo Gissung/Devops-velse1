af6a1e0fb6bd704570a57d35df3eb89270ca5d11
taarss <christiansps4@outlook.dk>
2025-02-17 12:02:35 +0100

    fixed layout

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,7 @@
-import globals from "globals";
-
+import globals from 'globals';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.browser } },
 ];
